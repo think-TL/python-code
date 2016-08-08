613a7321ad641356e5687f7fc8bdcd2935b14d74
think_tl <138792413@qq.com>
2016-08-08 12:56:12 +0800

    Update Test05_15.py

--- chapter5/Test05_15.py
+++ chapter5/Test05_15.py
@@ -1,28 +1,17 @@
-def least_multiple(x, y):
-   if x > y:
-        greater = x
-   else:
-        greater = y
-   while(True):
-        if((greater % x == 0) and (greater % y == 0)):
-            lcm = greater
-            break
-        greater += 1
-   return lcm
-
-num1 = int(input("Enter num1"))
-num2 = int(input("Enter num2"))
-print( num1,"and", num2,"Least common multiple", least_multiple(num1, num2))
-
-def hcf(a,b):
-    if a<b:
-        a,b=b,a
+def lcm(a, b):
+    if a < b:
+        a, b = b, a
     while b != 0:
-        temp = a%b
-        a=b
-        b=temp
+        temp = a % b
+        a = b
+        b = temp
     return a
 
 num1 = int(input("Enter num1"))
 num2 = int(input("Enter num2"))
-print( num1,"and", num2,"Greatest common divisor", hcf(num1, num2))
+print( num1,"and", num2,"Least common multiple", lcm(num1, num2))
+
+num1 = int(input("Enter num1"))
+num2 = int(input("Enter num2"))
+print lcm(num1,num2)
+print( num1,"and", num2,"Greatest common divisor", (num1*num2)/lcm(num1, num2))
