74d5a5b6088d8dafcb8c03af3862721d711b55c6
think_tl <138792413@qq.com>
2016-08-08 13:00:23 +0800

    Update Test5_05.py

--- chapter5/Test5_05.py
+++ chapter5/Test5_05.py
@@ -3,7 +3,7 @@ def changes(money):
     count = 0
     for i in money_list:
         yushu = divmod(money, i)
-        count += yushu[0]
+        count = yushu[0]
         money = yushu[1]
     print "%d a %d cnets" %(count, money_list[money])
 
